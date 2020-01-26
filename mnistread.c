cf23ee617d78941fed2e060d2685bdf90ad67fa2
jameseidson <eidsonje@icloud.com>
2020-01-26 00:27:47 -0500

    began mnist reader

---
+++ mnistread.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t readData(FILE *fp, double *in, double *expOut) {
+    /* TODO */
+}
+
+int main(int argc, char* argv[]) {
+    /* TODO */
+
+    return 0;
+}
